be9fdd7bae6865147aaac2851025063f1cf7e2cf
Sterpu Paul <paulsterpu@gmail.com>
2018-10-21 20:56:23 +0300

    Updates

---
+++ precache-manifest.26fc9cde12dbc232e8163fd4edd5a6f0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8896f9bb6a5a0506573fef60b5b4af6b",
+    "url": "/justBookIt/static/media/Catalin.8896f9bb.jpg"
+  },
+  {
+    "revision": "f83b44d7b0a0815c6701",
+    "url": "/justBookIt/static/css/main.03b599a5.chunk.css"
+  },
+  {
+    "revision": "bd4d612d329fad2a337b",
+    "url": "/justBookIt/static/js/1.bd4d612d.chunk.js"
+  },
+  {
+    "revision": "c7039aec7810cdb6b65a",
+    "url": "/justBookIt/static/js/runtime~main.c7039aec.js"
+  },
+  {
+    "revision": "360967812ccae14ba70b971f74784aa8",
+    "url": "/justBookIt/static/media/Anca.36096781.jpg"
+  },
+  {
+    "revision": "bab1d424b4bd70924dc63234cb0ca940",
+    "url": "/justBookIt/static/media/Radu.bab1d424.jpg"
+  },
+  {
+    "revision": "f83b44d7b0a0815c6701",
+    "url": "/justBookIt/static/js/main.f83b44d7.chunk.js"
+  },
+  {
+    "revision": "7852f67de128e1d702a4eff6d68c4c27",
+    "url": "/justBookIt/static/media/Daniel.7852f67d.jpg"
+  },
+  {
+    "revision": "8e1ac8f3f3903519408511a28331d6e1",
+    "url": "/justBookIt/static/media/Paul.8e1ac8f3.jpg"
+  },
+  {
+    "revision": "abdcf4857c18ad210e7ceb7eb82d6fe0",
+    "url": "/justBookIt/static/media/metrics.abdcf485.jpg"
+  },
+  {
+    "revision": "86fca744548e9c0100cda32603777a0f",
+    "url": "/justBookIt/static/media/revenue.86fca744.png"
+  },
+  {
+    "revision": "8df0521add6813427dd0421cddd4f59a",
+    "url": "/justBookIt/static/media/wallpaper.8df0521a.jpg"
+  },
+  {
+    "revision": "a3cadc57a95082a8360a17c67ab2deb6",
+    "url": "/justBookIt/index.html"
+  }
+];
